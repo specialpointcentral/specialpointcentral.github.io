b68231cd8087526625a42237489aab8eeb566f17
spcsky <spcreply@126.com>
2019-11-08 10:14:44 +0800

    Add the artical

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,54 +6,53 @@ function integrateGitalk(router) {
     const scriptGitalk = document.createElement('script');
     scriptGitalk.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js';
     document.body.appendChild(scriptGitalk);
-  
+
     router.afterEach((to) => {
-      if (scriptGitalk.onload) {
-        loadGitalk(to);
-      } else {
-        scriptGitalk.onload = () => {
-          loadGitalk(to);
+        if (scriptGitalk.onload) {
+            loadGitalk(to);
+        } else {
+            scriptGitalk.onload = () => {
+                loadGitalk(to);
+            }
         }
-      }
     });
-  
+
     function loadGitalk(to) {
-      let commentsContainer = document.getElementById('gitalk-container');
-      if (!commentsContainer) {
-        commentsContainer = document.createElement('div');
-        commentsContainer.id = 'gitalk-container';
-        commentsContainer.classList.add('content');
-      }
-      const $page = document.querySelector('.page');
-      if ($page) {
-        $page.appendChild(commentsContainer);
-        if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
-          renderGitalk(to.fullPath);
+        let commentsContainer = document.getElementById('gitalk-container');
+        if (!commentsContainer) {
+            commentsContainer = document.createElement('div');
+            commentsContainer.id = 'gitalk-container';
+            commentsContainer.classList.add('content');
         }
-      }
+        const $page = document.querySelector('.page');
+        if ($page) {
+            $page.appendChild(commentsContainer);
+            if (typeof Gitalk !== 'undefined' && Gitalk instanceof Function) {
+                renderGitalk(to.fullPath);
+            }
+        }
     }
     function renderGitalk(fullPath) {
-      const gitalk = new Gitalk({
-        clientID: 'bafa9288f91f1259ba98',
-        clientSecret: '3f81ac450bbbb87a6dd22207b31b96393fefa58c',
-        repo: 'specialpointcentral.github.io',
-        owner: 'specialpointcentral',
-        admin: ['specialpointcentral'],
-        id: md5(location.pathname),
-        title: document.title,
-        body:  '文章链接：'+ decodeURIComponent(location.origin+location.pathname),
-        distractionFreeMode: false,
-        language: 'zh-CN',
-      });
-      gitalk.render('gitalk-container');
+        const gitalk = new Gitalk({
+            clientID: 'bafa9288f91f1259ba98',
+            clientSecret: '3f81ac450bbbb87a6dd22207b31b96393fefa58c',
+            repo: 'specialpointcentral.github.io',
+            owner: 'specialpointcentral',
+            admin: ['specialpointcentral'],
+            id: md5(location.pathname),
+            title: document.title,
+            body: '文章链接：' + decodeURIComponent(location.origin + location.pathname),
+            distractionFreeMode: false,
+            language: 'zh-CN',
+        });
+        gitalk.render('gitalk-container');
     }
-  }
-  
-  export default ({Vue, options, router}) => {
+}
+
+export default ({ Vue, options, router }) => {
     try {
-      document && integrateGitalk(router)
+        document && integrateGitalk(router)
     } catch (e) {
-      console.error(e.message)
+        console.error(e.message)
     }
-  }
 }
